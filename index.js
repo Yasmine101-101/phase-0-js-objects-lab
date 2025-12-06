ba2613a4d4ec2a7bb57cdb3319e8f98ab1081639
yasmin <yasminmohamudyasmin@gmail.com>
2025-12-06 17:48:47 +0300

    in file index.js there was some changes made

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-//Write your code here
+
+let attendee = {
+  "attendeeId": "T001",
+  "name": "Alice Smith",
+  "event": "JavaScript Conference",
+  "ticketType": "VIP",
+  "ticketPrice": 150.00
+};  
+function logAttendeeName(attendee) {
+ 
+  console.log(attendee.name);
+}
+function logTicketPrice(attendee) {
+ 
+  console.log(attendee.ticketPrice);
+}
+function updateTicketType(attendee,newType) {
+ 
+  attendee.ticketType = newType;
+}
+function updateTicketPrice(attendee,newPrice) {
+attendee.ticketPrice = newPrice;
+}
+function removeEventProperty(attendee) { 
+  delete attendee.event;
+}
+function addCheckedInProperty(attendee) {
+  attendee.checkedIn = true;
+}
+
 
 
 
